ef58e4f358051e4bdc3942a230c90e1c3401124a
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-29 13:39:57 +0200

    feat(todo_app): creating some new function for the useage.

--- todo.py
+++ todo.py
@@ -12,7 +12,7 @@ def import_file(file_name):
         elif line[0] == '1':
             dictionary["complete"] = True
         task = line[2:]
-        dictionary['task'] = task[:-1]
+        dictionary['task'] = task
         items.append(dictionary)
     return items
 
@@ -28,21 +28,72 @@ def get_arguments():
         return arguments
     else:
         return False
-    
+
+def get_add(giving_item):
+    giving_item = argv[1:]
+    with open('todos.txt', 'a') as fw:
+        fw.write('0' + ''.join(giving_item) + '\n')
+    fw.close()
+    return import_file('todos.txt')
+
+def delete_items():
+        
+
+def check_task(items):
+    text = ""
+    i = 1
+    for line in items:
+        text += str(i) + " - "
+        print(line)
+        if line['complete'] == False:
+            text += '[ ] '
+        else:
+            text += '[X] '
+        text += line['task']
+        i += 1
+    print(text)
+            
 def controller():
     arguments = get_arguments()
+    todos = import_file('todos.txt')
+    # useage_command()
     if not arguments:
-        print("usage")
+        return 'Command line arguments: \n-l   Lists all the tasks\n-a   Adds a new task\n-r   Removes an task\
+                -c   Completes an task'
     elif arguments[0] == "-l":
-        todos = import_file('todos.txt')
         print(todos)
     elif arguments[0] == "-c":
-        print("completed")
+        check_task(todos)
     elif arguments[0] == "-r":
         print("remove")
     elif arguments[0] == "-a":
-        print("add")
+        todos = import_file('todos.txt')
+        todos = get_add('todos.txt')
+        print(todos)
+       
+        # print("add")
+
+
+
+# def useage_command():
+#     todos = import_file('todos.txt')
+#     arguments = get_arguments()
+#     text = ""
+#     if not arguments:
+#         return 'Command line arguments: \n-l   Lists all the tasks\n-a   Adds a new task\n-r   Removes an task\
+#                 -c   Completes an task'
+#     elif arguments[-1] == "-a" + 'text':
+#         todos.append("text")
+#         return (todos)
+#     elif arguments[-1] == "-l":
+#         print(todos)
+#     elif arguments[-1] == "-r" + str(len(todos[0])):
+#         todos.remove(todos[0])
+#         return todos    
+#     elif arguments[-1] == "-c" and str(len(todos[0])):
+#         todos[todos[0]] = int(len(todos[0]))
+
+
 
 controller()
-        
-
+# print(useage_command())
