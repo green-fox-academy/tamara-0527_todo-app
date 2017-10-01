611300139e74eaed13db228a00423dbbb9d1a38c
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-01 19:18:49 +0200

    feat(todo): get some new feature for the list task and adding new task parts

--- todo.py
+++ todo.py
@@ -16,27 +16,34 @@ def import_file(file_name):
         items.append(dictionary)
     return items
 
+def print_items(todos):
+    if len(todos) == 0:
+       print("No todos for today! :)")
+    else:
+        for index in range(len(todos)):
+            print(str(index+1) + ' - ' + todos[index]['task']) 
 
 todo = argv[0]
 
 def get_arguments():
     arguments = argv[1:]
     list_of_arguments = ['-a', '-r', '-c', '-l']
-    if len(arguments) == 0:
-        return False
-    if arguments[0] in list_of_arguments:
+    if len(arguments) > 0 and arguments[0] in list_of_arguments:
         return arguments
     else:
         return False
 
 def get_add(giving_item):
-    giving_item = argv[1:]
-    with open('todos.txt', 'a') as fw:
-        fw.write('0' + ''.join(giving_item) + '\n')
-    fw.close()
-    return import_file('todos.txt')
+    if len(argv) >= 3:
+        giving_items = argv[2:]
+        with open('todos.txt', 'a') as fw:
+            fw.write('0 ' + ' '.join(giving_items) + '\n')
+        fw.close()
+        return import_file('todos.txt')
+    else:
+        print("Unable to add: no task provided")
 
-def delete_items():
+# def delete_items():
         
 
 def check_task(items):
@@ -58,22 +65,21 @@ def controller():
     todos = import_file('todos.txt')
     # useage_command()
     if not arguments:
-        return 'Command line arguments: \n-l   Lists all the tasks\n-a   Adds a new task\n-r   Removes an task\
-                -c   Completes an task'
+        print_usage()
     elif arguments[0] == "-l":
-        print(todos)
+        print_items(todos)
     elif arguments[0] == "-c":
         check_task(todos)
     elif arguments[0] == "-r":
         print("remove")
     elif arguments[0] == "-a":
-        todos = import_file('todos.txt')
         todos = get_add('todos.txt')
-        print(todos)
+        
        
         # print("add")
 
-
+def print_usage():
+    print('Command line arguments: \n-l   Lists all the tasks\n-a   Adds a new task\n-r   Removes an task\n-c   Completes an task')
 
 # def useage_command():
 #     todos = import_file('todos.txt')
